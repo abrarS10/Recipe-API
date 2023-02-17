b24ea4692a4197167003fc2f15f688df2877ad8f
abrar_sajeel <abrarsajeel@gmail.com>
2023-02-17 21:23:14 +0500

    Set up postgresql docker image and configured in django

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """Return the sum of 2 numbers"""
+    return x + y
+
+def subtract(x,y):
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(6, 6)
+
+        self.assertEqual(res, 12)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
