539e136ea894cc6762834ab49dd900dae585530d
abrar_sajeel <abrarsajeel@gmail.com>
2023-02-21 12:35:01 +0500

    Added update user endpoint + tests

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 Views for the user API
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -11,7 +11,7 @@ from user.serializers import (
     AuthTokenSerializer
 )
 
-class CreateUserView(generics.CreateAPIView):
+class CreateUserView(generics.CreateAPIView): # provided by drf for creating new objects in the DB
     """Create a new user in the system"""
     # django sets relevant serializer to serializer_class
     serializer_class = UserSerializer
@@ -20,3 +20,13 @@ class CreateTokenView(ObtainAuthToken):
     """create a new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView): # provided by drf for retrieving and updating objects in the DB
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self): # overriding default get object method
+        """Retrieve and returh the authenticated user"""
+        return self.request.user
