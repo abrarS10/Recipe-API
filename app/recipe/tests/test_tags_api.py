1dca34727976435c4dd3f9f0670bcf46c8ca5a52
abrar_sajeel <abrarsajeel@gmail.com>
2023-02-23 20:01:00 +0500

    Added functionality for deleting and updating tags + tests

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -89,16 +89,16 @@ class PrivateTagsApiTests(TestCase):
         tag.refresh_from_db()
         self.assertEqual(tag.name, payload['name'])
 
-    # def test_delete_tag(self):
-    #     """Test deleting a tag."""
-    #     tag = Tag.objects.create(user=self.user, name='Breakfast')
+    def test_delete_tag(self):
+        """Test deleting a tag."""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
 
-    #     url = detail_url(tag.id)
-    #     res = self.client.delete(url)
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
 
-    #     self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
-    #     tags = Tag.objects.filter(user=self.user)
-    #     self.assertFalse(tags.exists())
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
 
     # def test_filter_tags_assigned_to_recipes(self):
     #     """Test listing tags to those assigned to recipes."""
